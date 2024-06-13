db9fd2daa118fd5df00d58d2d28e4ed6207a5363
akbarsenawjy26 <wijayasenaakbar@gmail.com>
2024-06-14 00:04:09 +0700

    feat(controller): update program controller untuk simpan data ke influxdb

--- controllers/modbusController.js
+++ controllers/modbusController.js
@@ -1,8 +1,21 @@
 const ModbusModel = require("../models/modbusModel");
+//influxdb
+const { InfluxDB, Point } = require("@influxdata/influxdb-client");
+const token =
+  "jfHVFte40Q937UgC6_v62iDdYvHHPWC1HIi_Ifpl8y3MjnsD634l-KedjmDzWBqpWR32mFSwxISMXPHY5p8a-A==";
+const url = "http://localhost:8086";
+
+const client = new InfluxDB({ url, token });
+
+const writeApi = client.getWriteApi("WIT.SBY", "BusDuct");
 
 const slaves = [
   { id: 58, ip: "192.168.17.111" },
   { id: 59, ip: "192.168.17.111" },
+  { id: 60, ip: "192.168.17.111" },
+  { id: 61, ip: "192.168.17.111" },
+  { id: 62, ip: "192.168.17.111" },
+  { id: 63, ip: "192.168.17.111" },
 ];
 
 let cachedData = {};
@@ -19,6 +32,91 @@ const fetchData = async (slave) => {
   try {
     const data = await modbus.readRegisters(0, 80, slave.id); // Adjusted the number of registers to 80
     console.log(slave.id, " : ", data);
+
+    //INFLUX SAVE
+    const points = [];
+
+    // Phase 1
+    const phase1Point = new Point("modbus_data")
+      .tag("phase", "phase1")
+      .tag("slaveId", slave.id.toString())
+      .floatField("voltageLN", convertRegistersToFloat(data[0], data[1]))
+      .floatField("voltageLL", convertRegistersToFloat(data[2], data[3]))
+      .floatField("current", convertRegistersToFloat(data[4], data[5]))
+      .floatField("cosPhi", convertRegistersToFloat(data[6], data[7]))
+      .floatField("pf", convertRegistersToFloat(data[8], data[9]))
+      .floatField("activePower", convertRegistersToFloat(data[10], data[11]))
+      .floatField("reactivePower", convertRegistersToFloat(data[12], data[13]))
+      .floatField("apparentPower", convertRegistersToFloat(data[14], data[15]))
+      .floatField("thdv", convertRegistersToFloat(data[16], data[17]))
+      .floatField("thdi", convertRegistersToFloat(data[18], data[19]))
+      .timestamp(new Date());
+
+    points.push(phase1Point);
+
+    // Phase 2
+    const phase2Point = new Point("modbus_data")
+      .tag("phase", "phase2")
+      .tag("slaveId", slave.id.toString())
+      .floatField("voltageLN", convertRegistersToFloat(data[20], data[21]))
+      .floatField("voltageLL", convertRegistersToFloat(data[22], data[23]))
+      .floatField("current", convertRegistersToFloat(data[24], data[25]))
+      .floatField("cosPhi", convertRegistersToFloat(data[26], data[27]))
+      .floatField("pf", convertRegistersToFloat(data[28], data[29]))
+      .floatField("activePower", convertRegistersToFloat(data[30], data[31]))
+      .floatField("reactivePower", convertRegistersToFloat(data[32], data[33]))
+      .floatField("apparentPower", convertRegistersToFloat(data[34], data[35]))
+      .floatField("thdv", convertRegistersToFloat(data[36], data[37]))
+      .floatField("thdi", convertRegistersToFloat(data[38], data[39]))
+      .timestamp(new Date());
+
+    points.push(phase2Point);
+
+    // Phase 3
+    const phase3Point = new Point("modbus_data")
+      .tag("phase", "phase3")
+      .tag("slaveId", slave.id.toString())
+      .floatField("voltageLN", convertRegistersToFloat(data[40], data[41]))
+      .floatField("voltageLL", convertRegistersToFloat(data[42], data[43]))
+      .floatField("current", convertRegistersToFloat(data[44], data[45]))
+      .floatField("cosPhi", convertRegistersToFloat(data[46], data[47]))
+      .floatField("pf", convertRegistersToFloat(data[48], data[49]))
+      .floatField("activePower", convertRegistersToFloat(data[50], data[51]))
+      .floatField("reactivePower", convertRegistersToFloat(data[52], data[53]))
+      .floatField("apparentPower", convertRegistersToFloat(data[54], data[55]))
+      .floatField("thdv", convertRegistersToFloat(data[56], data[57]))
+      .floatField("thdi", convertRegistersToFloat(data[58], data[59]))
+      .timestamp(new Date());
+
+    points.push(phase3Point);
+
+    // Common data
+    const commonPoint = new Point("modbus_data")
+      .tag("phase", "common")
+      .tag("slaveId", slave.id.toString())
+      .floatField("totalCurrent", convertRegistersToFloat(data[60], data[61]))
+      .floatField("totalPF", convertRegistersToFloat(data[62], data[63]))
+      .floatField(
+        "totalActivePower",
+        convertRegistersToFloat(data[64], data[65])
+      )
+      .floatField(
+        "totalReactivePower",
+        convertRegistersToFloat(data[66], data[67])
+      )
+      .floatField(
+        "totalApparentPower",
+        convertRegistersToFloat(data[68], data[69])
+      )
+      .floatField("totalFreq", convertRegistersToFloat(data[74], data[75]))
+      .floatField("NeutralCurrent", convertRegistersToFloat(data[76], data[77]))
+      .timestamp(new Date());
+
+    points.push(commonPoint);
+
+    // Menulis semua titik (points) ke InfluxDB
+    writeApi.writePoints(points);
+    //################################################
     const result = {
       phase1: {
         voltageLN: convertRegistersToFloat(data[0], data[1]),
