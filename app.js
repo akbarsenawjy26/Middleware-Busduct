02090bfe90c72acfbc62a9752ec051b2df70c1d3
akbarsenawjy26 <wijayasenaakbar@gmail.com>
2024-06-13 23:59:25 +0700

    feat(app.js): update restAPI untuk get data dari influxdb

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3001;
 
 const modbusRoutes = require("./routes/modbusRoutes");
+const influxRoutes = require("./routes/influxdbRoute");
 
 app.use("/api", modbusRoutes);
+app.use("/api", influxRoutes);
 
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
